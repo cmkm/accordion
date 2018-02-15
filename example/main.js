bf28e51536700095b420af97caa45fe45d7267fe
Cieara Meador <cmkmeador@gmail.com>
2018-02-15 10:06:47 -0500

    customTargets extension

--- example/main.js
+++ example/main.js
@@ -1,27 +1,27 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function(){function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s}return e})()({1:[function(require,module,exports){
 'use strict';
 
 var accordion = require('..');
 
 var accordionElement = document.querySelector('.js-accordion');
 
-new accordion.Accordion(accordionElement, {}, {openFirst: true});
+new accordion.Accordion(accordionElement, {}, {openFirst: true, customTargets: true});
 
 },{"..":2}],2:[function(require,module,exports){
 'use strict';
 
 var extend = require('./util').extend;
-
 var defaultOpts = {
-  collapseOthers: false,
-  customHiding: false,
-  contentPrefix: 'accordion',
-  openFirst: false,
-  reflectStatic: false
+    collapseOthers: false,
+    customHiding: false,
+    customTargets: false,
+    contentPrefix: 'accordion',
+    openFirst: false,
+    reflectStatic: false
 };
 
 var defaultSelectors = {
-  trigger: 'button'
+    trigger: 'button'
 };
 
 /**
@@ -32,131 +32,140 @@ var defaultSelectors = {
  * @param {object} opts - Options for configuring behavior
  */
 
-var Accordion = function(elm, selectors, opts) {
-  this.elm = elm;
-  this.selectors = extend({}, defaultSelectors, selectors);
-  this.opts = extend({}, defaultOpts, opts);
+var Accordion = function (elm, selectors, opts) {
+    this.elm = elm;
+    this.selectors = extend({}, defaultSelectors, selectors);
+    this.opts = extend({}, defaultOpts, opts);
 
-  this.triggers = this.findTriggers();
+    this.triggers = this.findTriggers();
 
-  this.listeners = [];
-  this.addEventListener(this.elm, 'click', this.handleClickElm.bind(this));
+    this.listeners = [];
+    this.addEventListener(this.elm, 'click', this.handleClickElm.bind(this));
 
-  if (this.opts.openFirst) {
-    this.expand(this.triggers[0]);
-  }
+    if (this.opts.openFirst) {
+        this.expand(this.triggers[0]);
+    }
 };
 
-Accordion.prototype.handleClickElm = function(e) {
-  // If the target is the button, toggle the button
-  // Else see if the target is a child of a button
-  if (this.triggers.indexOf(e.target) > -1) {
-    this.toggle(e.target);
-  } else {
+Accordion.prototype.handleClickElm = function (e) {
+    // If the target is the button, toggle the button
+    // Else see if the target is a child of a button
+    if (this.triggers.indexOf(e.target) > -1) {
+        this.toggle(e.target);
+    } else {
+        var self = this;
+        this.triggers.forEach(function (trigger) {
+            if (e.target.parentElement === trigger) {
+                self.toggle(trigger);
+            }
+        });
+    }
+};
+
+Accordion.prototype.findTriggers = function () {
     var self = this;
-    this.triggers.forEach(function(trigger){
-      if (e.target.parentElement === trigger) {
-        self.toggle(trigger);
-      }
+    var triggers = [].slice.call(this.elm.querySelectorAll(this.selectors.trigger));
+    triggers.forEach(function (trigger, index) {
+        self.setAria(trigger, index);
     });
-  }
+    return triggers;
 };
 
-Accordion.prototype.findTriggers = function() {
-  var self = this;
-  var triggers = [].slice.call(this.elm.querySelectorAll(this.selectors.trigger));
-  triggers.forEach(function(trigger, index) {
-    self.setAria(trigger, index);
-  });
-  return triggers;
+Accordion.prototype.setAria = function (trigger, index) {
+    var contentID;
+    var content;
+
+    if (this.opts.customTargets && trigger.hasAttribute('aria-controls')) {
+    // if (this.opts.customTargets) {
+        contentID = trigger.getAttribute('aria-controls');
+        content = document.getElementById(contentID);
+    } else {
+        content = trigger.nextElementSibling;
+    }
+
+    var initExpanded = 'false';
+    var initHidden = 'true';
+
+    if (content.hasAttribute('id')) {
+        contentID = content.getAttribute('id');
+    } else {
+        contentID = this.opts.contentPrefix + '-' + 'content-' + index;
+        content.setAttribute('id', contentID);
+    }
+
+    if (this.opts.reflectStatic) {
+        initExpanded = trigger.getAttribute('aria-expanded') || initExpanded;
+        initHidden = content.getAttribute('aria-hidden') || initHidden;
+    }
+
+    trigger.setAttribute('aria-controls', contentID);
+    trigger.setAttribute('aria-expanded', initExpanded);
+    content.setAttribute('aria-hidden', initHidden);
+    this.setStyles(content);
 };
 
-Accordion.prototype.setAria = function(trigger, index) {
-  var content = trigger.nextElementSibling;
-  var contentID;
-  var initExpanded = 'false';
-  var initHidden = 'true';
-
-  if (content.hasAttribute('id')) {
-    contentID = content.getAttribute('id');
-  } else {
-    contentID = this.opts.contentPrefix + '-' + 'content-' + index;
-    content.setAttribute('id', contentID);
-  }
-
-  if (this.opts.reflectStatic) {
-    initExpanded = trigger.getAttribute('aria-expanded') || initExpanded;
-    initHidden = content.getAttribute('aria-hidden') || initHidden;
-  }
-
-  trigger.setAttribute('aria-controls', contentID);
-  trigger.setAttribute('aria-expanded', initExpanded);
-  content.setAttribute('aria-hidden', initHidden);
-  this.setStyles(content);
+Accordion.prototype.toggle = function (elm) {
+    var f = elm.getAttribute('aria-expanded') === 'true' ? this.collapse : this.expand;
+    f.call(this, elm);
 };
 
-Accordion.prototype.toggle = function(elm) {
-  var f = elm.getAttribute('aria-expanded') === 'true' ? this.collapse : this.expand;
-  f.call(this, elm);
+Accordion.prototype.expand = function (button) {
+    if (this.opts.collapseOthers) {
+        this.collapseAll();
+    }
+    var content = document.getElementById(button.getAttribute('aria-controls'));
+    button.setAttribute('aria-expanded', 'true');
+    content.setAttribute('aria-hidden', 'false');
+    this.setStyles(content);
 };
 
-Accordion.prototype.expand = function(button) {
-  if (this.opts.collapseOthers) {
-    this.collapseAll();
-  }
-  var content = document.getElementById(button.getAttribute('aria-controls'));
-  button.setAttribute('aria-expanded', 'true');
-  content.setAttribute('aria-hidden', 'false');
-  this.setStyles(content);
+Accordion.prototype.collapse = function (button) {
+    var content = document.getElementById(button.getAttribute('aria-controls'));
+    button.setAttribute('aria-expanded', 'false');
+    content.setAttribute('aria-hidden', 'true');
+    this.setStyles(content);
 };
 
-Accordion.prototype.collapse = function(button) {
-  var content = document.getElementById(button.getAttribute('aria-controls'));
-  button.setAttribute('aria-expanded', 'false');
-  content.setAttribute('aria-hidden', 'true');
-  this.setStyles(content);
-};
-
-Accordion.prototype.collapseAll = function() {
-  var self = this;
-  this.triggers.forEach(function(trigger) {
-    self.collapse(trigger);
-  });
-};
-
-Accordion.prototype.expandAll = function() {
-  var self = this;
-  this.triggers.forEach(function(trigger) {
-    self.expand(trigger);
-  });
-};
-
-Accordion.prototype.setStyles = function(content) {
-  var prop = content.getAttribute('aria-hidden') === 'true' ? 'none' : 'block';
-
-  if (!this.opts.customHiding) {
-    content.style.display = prop;
-  }
-};
-
-Accordion.prototype.addEventListener = function(elm, event, callback) {
-  if (elm) {
-    elm.addEventListener(event, callback);
-    this.listeners.push({
-      elm: elm,
-      event: event,
-      callback: callback
+Accordion.prototype.collapseAll = function () {
+    var self = this;
+    this.triggers.forEach(function (trigger) {
+        self.collapse(trigger);
     });
-  }
 };
 
-Accordion.prototype.destroy = function() {
-  this.listeners.forEach(function(listener) {
-    listener.elm.removeEventListener(listener.event, listener.callback);
-  });
+Accordion.prototype.expandAll = function () {
+    var self = this;
+    this.triggers.forEach(function (trigger) {
+        self.expand(trigger);
+    });
 };
 
-module.exports = { Accordion: Accordion };
+Accordion.prototype.setStyles = function (content) {
+    var prop = content.getAttribute('aria-hidden') === 'true' ? 'none' : 'block';
+
+    if (!this.opts.customHiding) {
+        content.style.display = prop;
+    }
+};
+
+Accordion.prototype.addEventListener = function (elm, event, callback) {
+    if (elm) {
+        elm.addEventListener(event, callback);
+        this.listeners.push({
+            elm: elm,
+            event: event,
+            callback: callback
+        });
+    }
+};
+
+Accordion.prototype.destroy = function () {
+    this.listeners.forEach(function (listener) {
+        listener.elm.removeEventListener(listener.event, listener.callback);
+    });
+};
+
+module.exports = {Accordion: Accordion};
 
 },{"./util":3}],3:[function(require,module,exports){
 var extend = function(out) {
